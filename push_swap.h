1184053eff0438864b19f46cc737b267e2805e76
Paixex <puto.gil.18@gmail.com>
2023-10-10 16:24:56 +0100

    .

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: digil-pa <digil-pa@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 17:56:47 by digil-pa          #+#    #+#             */
-/*   Updated: 2023/10/10 13:49:09 by digil-pa         ###   ########.fr       */
+/*   Updated: 2023/10/10 16:24:48 by digil-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,59 +40,59 @@ typedef struct node_t
 
 //LISTS
 
-int			ft_lstsize(t_node *lst);
-t_node		*ft_lstlast(t_node *lst);
-t_node		*ft_lstadd_back(t_node **lst, t_node *new);
-void		ft_lstadd_front(t_node **lst, t_node *new);
-t_node		*ft_lstnew(int content);
-void		ft_clean_lst(t_node **stack);
+int					ft_lstsize(t_node *lst);
+t_node				*ft_lstlast(t_node *lst);
+t_node				*ft_lstadd_back(t_node **lst, t_node *new);
+void				ft_lstadd_front(t_node **lst, t_node *new);
+t_node				*ft_lstnew(int content);
+void				ft_clean_lst(t_node **stack);
 
 //SORT
 
-void		sort_small_3(t_node **stack_a);
-void		sort_small_5(t_node **stack_a, t_node **stack_b);
-void		sort_all(t_node	**stack_a, t_node **stack_b);
-void		sort_big(t_node **stack_a, t_node **stack_b);
-void		sort_b(t_node **stack_a, t_node **stack_b, int max);
-void		sortsmall_b(t_node **stack_a, int val_2, int val_3);
-void		sortsmall_a(t_node **stack_a, int val_1, int val_3);
-void		sort_2(t_node **stack_a);
-void		sort(t_node **stack_a, t_node **stack_b, int ac);
+void				sort_small_3(t_node **stack_a);
+void				sort_small_5(t_node **stack_a, t_node **stack_b);
+void				sort_all(t_node	**stack_a, t_node **stack_b);
+void				sort_big(t_node **stack_a, t_node **stack_b);
+void				sort_b(t_node **stack_a, t_node **stack_b, int max);
+void				sortsmall_b(t_node **stack_a, int val_2, int val_3);
+void				sortsmall_a(t_node **stack_a, int val_1, int val_3);
+void				sort_2(t_node **stack_a);
+void				sort(t_node **stack_a, t_node **stack_b, int ac);
 //CHECKERS
 
-int			ft_isdigit(int k);
-int			signal_check(int ac, char **av);
-int			digit_checker(int ac, char **av);
-int			check_int_val(int ac, char **av);
-int			check_if_ok(t_node **stack);
-int			check_for_rep(int ac, char **av);
-int			check_input(int ac, char **av);
-int			okok(t_node **stack);
+int					ft_isdigit(int k);
+int					signal_check(int ac, char **av);
+int					digit_checker(int ac, char **av);
+int					check_int_val(int ac, char **av);
+int					check_if_ok(t_node **stack);
+int					check_for_rep(int ac, char **av);
+int					check_input(int ac, char **av);
+int					okok(t_node **stack);
 
 //MOVES
 
-void		push_back_to_a(t_node **stack_a, t_node **stack_b);
-void		best_mv(t_node **stack_a, t_node **stack_b);
-void		mv_to_b(int n, t_node **stack_a, t_node **stack_b);
-void		put_nr_top_a(t_node **stack, int nr_to_mv);
-void		put_nr_top_b(t_node **stack, int nr_to_mv);
-void		put_on_top(t_node **stack, int i);
-void		push_b(t_node **stack_a, t_node **stack_b);
-void		push_a(t_node **stack_a, t_node **stack_b);
-void		ft_ra(t_node **stack_a);
-void		ft_rb(t_node **stack_b);
-void		ft_rr(t_node **stack_a, t_node **stack_b);
-void		ft_sa(t_node **stack_a);
-void		ft_sb(t_node **stack_b);
-void		ft_ss(t_node **stack_a, t_node **stack_b);
-void		ft_rra(t_node **stack_a);
-void		ft_rrb(t_node **stack_b);
-void		ft_rrr(t_node **stack_a, t_node **stack_b);
+void				push_back_to_a(t_node **stack_a, t_node **stack_b);
+void				best_mv(t_node **stack_a, t_node **stack_b);
+void				mv_to_b(int n, t_node **stack_a, t_node **stack_b);
+void				put_nr_top_a(t_node **stack, int nr_to_mv);
+void				put_nr_top_b(t_node **stack, int nr_to_mv);
+void				put_on_top(t_node **stack, int i);
+void				push_b(t_node **stack_a, t_node **stack_b);
+void				push_a(t_node **stack_a, t_node **stack_b);
+void				ft_ra(t_node **stack_a);
+void				ft_rb(t_node **stack_b);
+void				ft_rr(t_node **stack_a, t_node **stack_b);
+void				ft_sa(t_node **stack_a);
+void				ft_sb(t_node **stack_b);
+void				ft_ss(t_node **stack_a, t_node **stack_b);
+void				ft_rra(t_node **stack_a);
+void				ft_rrb(t_node **stack_b);
+void				ft_rrr(t_node **stack_a, t_node **stack_b);
 
 //PRINTS
 
-t_node		*insert_stack_a(char **av);
-void		print_stack(t_node *head);
+t_node				*insert_stack_a(char **av);
+void				print_stack(t_node *head);
 
 //FINDS
 
